e8674ae1824654fc92b1fe53d74921e378f07596
Peter Jones <36066Pe@YPM2361.local>
2014-07-07 10:28:19 +0100

    Get all files up to date (including the .htaccess)

--- pushcontact/api/api_config.php
+++ pushcontact/api/api_config.php
@@ -7,10 +7,10 @@ $config = array(
 	// These are the settings for development mode
 	'development' => array(
 		'db' => array(
-			'host'     => 'db6.awardspace.net',
-			'dbname'   => '1698700_foo',
-			'username' => '1698700_foo',
-			'password' => '6NY-3J4-PYH-5rW',
+			'host'     => 'mysql.hostinger.co.uk',
+			'dbname'   => 'u163695907_pct',
+			'username' => 'u163695907_pct',
+			'password' => 'ENLm0ThILYMWZ',
 			),
 		),
 
